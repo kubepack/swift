06cebb71df19df8f22f45417afeb298da8a4e232
tamal <tamal@appscode.com>
2017-09-20 20:39:34 -0700

    Check for returned pods or services before connecting.
    Fixes potential NPE.

--- pkg/factory/tunnel_connector.go
+++ pkg/factory/tunnel_connector.go
@@ -1,6 +1,7 @@
 package factory
 
 import (
+	"errors"
 	"fmt"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -19,6 +20,10 @@ func (s *TunnelConnector) GetTillerAddr(client clientset.Interface, config *rest
 	if err != nil {
 		return "", err
 	}
+	if len(podList.Items) == 0 {
+		return "", errors.New("no tiller pod(s) found")
+	}
+
 	tunnel := newTunnel(client.CoreV1().RESTClient(), config, podList.Items[0].Namespace, podList.Items[0].Name, defaultTillerPort)
 	if err := tunnel.forwardPort(); err != nil {
 		return "", err
