e972a30aac28f019ce4684bd118d9f6b8d5a2eb3
Tamal Saha <tamal@appscode.com>
2018-08-18 10:57:48 -0700

    Format shell scripts `find hack | grep sh | xargs shfmt -l -w -ci -i 2 {}`
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/docker/setup.sh
+++ hack/docker/setup.sh
@@ -19,29 +19,29 @@ IMG=swift
 DIST=$GOPATH/src/github.com/appscode/swift/dist
 mkdir -p $DIST
 if [ -f "$DIST/.tag" ]; then
-	export $(cat $DIST/.tag | xargs)
+  export $(cat $DIST/.tag | xargs)
 fi
 
 clean() {
-    pushd $GOPATH/src/github.com/appscode/swift/hack/docker
-    rm swift Dockerfile
-    popd
+  pushd $GOPATH/src/github.com/appscode/swift/hack/docker
+  rm swift Dockerfile
+  popd
 }
 
 build_binary() {
-    pushd $GOPATH/src/github.com/appscode/swift
-    ./hack/builddeps.sh
-    ./hack/make.py build
-    detect_tag $DIST/.tag
-    popd
+  pushd $GOPATH/src/github.com/appscode/swift
+  ./hack/builddeps.sh
+  ./hack/make.py build
+  detect_tag $DIST/.tag
+  popd
 }
 
 build_docker() {
-    pushd $GOPATH/src/github.com/appscode/swift/hack/docker
-    cp $DIST/swift/swift-alpine-amd64 swift
-    chmod 755 swift
+  pushd $GOPATH/src/github.com/appscode/swift/hack/docker
+  cp $DIST/swift/swift-alpine-amd64 swift
+  chmod 755 swift
 
-    cat >Dockerfile <<EOL
+  cat >Dockerfile <<EOL
 FROM alpine
 
 RUN set -x \
@@ -52,40 +52,40 @@ COPY swift /usr/bin/swift
 USER nobody:nobody
 ENTRYPOINT ["swift"]
 EOL
-    local cmd="docker build -t appscode/$IMG:$TAG ."
-    echo $cmd; $cmd
+  local cmd="docker build -t appscode/$IMG:$TAG ."
+  echo $cmd; $cmd
 
-    rm swift Dockerfile
-    popd
+  rm swift Dockerfile
+  popd
 }
 
 build() {
-    build_binary
-    build_docker
+  build_binary
+  build_docker
 }
 
 docker_push() {
-    if [ "$APPSCODE_ENV" = "prod" ]; then
-        echo "Nothing to do in prod env. Are you trying to 'release' binaries to prod?"
-        exit 0
-    fi
-    if [ "$TAG_STRATEGY" = "git_tag" ]; then
-        echo "Are you trying to 'release' binaries to prod?"
-        exit 1
-    fi
-    hub_canary
+  if [ "$APPSCODE_ENV" = "prod" ]; then
+    echo "Nothing to do in prod env. Are you trying to 'release' binaries to prod?"
+    exit 0
+  fi
+  if [ "$TAG_STRATEGY" = "git_tag" ]; then
+    echo "Are you trying to 'release' binaries to prod?"
+    exit 1
+  fi
+  hub_canary
 }
 
 docker_release() {
-    if [ "$APPSCODE_ENV" != "prod" ]; then
-        echo "'release' only works in PROD env."
-        exit 1
-    fi
-    if [ "$TAG_STRATEGY" != "git_tag" ]; then
-        echo "'apply_tag' to release binaries and/or docker images."
-        exit 1
-    fi
-    hub_up
+  if [ "$APPSCODE_ENV" != "prod" ]; then
+    echo "'release' only works in PROD env."
+    exit 1
+  fi
+  if [ "$TAG_STRATEGY" != "git_tag" ]; then
+    echo "'apply_tag' to release binaries and/or docker images."
+    exit 1
+  fi
+  hub_up
 }
 
 source_repo $@
